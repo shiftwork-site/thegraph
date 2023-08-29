b8ddef27735e8e435a6e232f6bd8d58dde3584a1
rike <hello@rike.dev>
2023-08-29 23:42:42 +0200

    initial commit

---
+++ src/shiftcaptures.ts
@@ -0,0 +1,107 @@
+import {
+  ApprovalForAll as ApprovalForAllEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  TokenMinted as TokenMintedEvent,
+  TransferBatch as TransferBatchEvent,
+  TransferSingle as TransferSingleEvent,
+  URI as URIEvent
+} from "../generated/SHIFTCAPTURES/SHIFTCAPTURES"
+import {
+  ApprovalForAll,
+  OwnershipTransferred,
+  TokenMinted,
+  TransferBatch,
+  TransferSingle,
+  URI
+} from "../generated/schema"
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.account = event.params.account
+  entity.operator = event.params.operator
+  entity.approved = event.params.approved
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTokenMinted(event: TokenMintedEvent): void {
+  let entity = new TokenMinted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.tokenId = event.params.tokenId
+  entity.minter = event.params.minter
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransferBatch(event: TransferBatchEvent): void {
+  let entity = new TransferBatch(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.operator = event.params.operator
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.ids = event.params.ids
+  entity.values = event.params.values
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransferSingle(event: TransferSingleEvent): void {
+  let entity = new TransferSingle(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.operator = event.params.operator
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.SHIFTCAPTURES_id = event.params.id
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleURI(event: URIEvent): void {
+  let entity = new URI(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  entity.value = event.params.value
+  entity.SHIFTCAPTURES_id = event.params.id
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
